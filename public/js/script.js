bba81da5437a4aa05366b6bbddb1bfbcd96b910b
Seita Uchimura <c201552k@gmail.com>
2019-04-02 04:39:56 -0700

    :watermelon: Add:circleciのビルドコマンドに--ignoreCacheを追加

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const toc = document.getElementById('table-of-contents');
-    const tocPosition = document.getElementById("toc-position");
-
-    if (tocPosition != null) {
-        tocPosition.appendChild(toc);
-    }
-
-})();
