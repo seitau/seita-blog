ad65d04d958039df77e86e7add3693c8ae0868f5
Seita Uchimura <c201552k@gmail.com>
2019-04-19 18:30:47 -0700

    :watermelon: Add:profileに合わせてp5のcanvasを設定

--- static/js/src/main.js
+++ static/js/src/main.js
@@ -1,18 +1,22 @@
 import 'babel-polyfill';
 
+let canvas;
 const sketch = function(p5) {
     p5.setup = async function() {
-        p5.createCanvas(p5.windowWidth, p5.windowHeight);
-        p5.background(0);
-        console.log('hoge');
+        const profile = document.getElementById("profile-inner");
+        canvas = p5.createCanvas(profile.clientWidth, profile.clientHeight);
+        canvas.parent('profile-inner');
+        canvas.position(0, 0);
+        canvas.style('z-index', '-3');
     }
 
     p5.draw = async function() {
-        p5.background(0);
+        p5.background('#fff');
     }
 
     p5.windowResized = function() {
-        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+        const profile = document.getElementById("profile-inner");
+        p5.resizeCanvas(profile.clientWidth, profile.clientHeight);
     }
 
     p5.mousePressed = function() {
