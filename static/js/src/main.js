8fb8dfa74ea8d2b0ee49cf488b09c5da1c199f95
Seita Uchimura <c201552k@gmail.com>
2019-04-19 17:34:27 -0700

    :watermelon: Add:p5jsを導入

---
+++ static/js/src/main.js
@@ -0,0 +1,25 @@
+import 'babel-polyfill';
+
+const sketch = function(p5) {
+    p5.setup = async function() {
+        p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        p5.background(0);
+    }
+
+    p5.draw = async function() {
+        p5.background(0);
+    }
+
+    p5.windowResized = function() {
+        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    }
+
+    p5.mousePressed = function() {
+    }
+
+    p5.mouseWheel = function(event) {
+    }
+}
+
+new p5(sketch)
+
